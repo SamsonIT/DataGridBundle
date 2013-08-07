7a23bad799ebe24bd63eb1db5d99e6b3237b51b1
Bart van den Burg <bart@samson-it.nl>
2013-08-07 15:47:05 +0200

    stub for adding options later on

--- Helper/ControllerHelperConfiguration.php
+++ Helper/ControllerHelperConfiguration.php
@@ -3,16 +3,23 @@
 namespace Samson\Bundle\DataGridBundle\Helper;
 
 use Samson\Bundle\DataViewBundle\DataView\AbstractDataView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class ControllerHelperConfiguration
 {
     private $view;
     private $formType;
+    private $options;
 
-    public function __construct(AbstractDataView $view, $formType)
+    public function __construct(AbstractDataView $view, $formType, array $options = array())
     {
         $this->formType = $formType;
         $this->view = $view;
+
+        $resolver = new OptionsResolver();
+        $this->setDefaultOptions($resolver);
+        $this->options = $resolver->resolve($options);
     }
 
     public function getFormType()
@@ -23,6 +30,17 @@ class ControllerHelperConfiguration
     public function getView()
     {
         return $this->view;
+    }
+
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+        ));
     }
 
 }
